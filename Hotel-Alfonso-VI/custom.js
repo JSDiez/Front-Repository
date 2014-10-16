e19f0252ba84e966f2195ed891c55386030e212a
JSDiez <hello@jsdiez.es>
2014-10-16 12:53:11 +0200

    Update Js
    add dynamic offset to affix

--- Hotel-Alfonso-VI/custom.js
+++ Hotel-Alfonso-VI/custom.js
@@ -6,4 +6,10 @@ Mirai.query('#engine a').click(function(evt){
   var link = Mirai.query(this).attr('href');
   var pos = Mirai.query(link).offset().top;
   Mirai.query('html,body').animate({scrollTop:pos-50},700);
-}); 
+});
+
+Mirai.query('#hotel-menu').affix({
+		offset: {
+		top: Mirai.query("#header").height() + Mirai.query("#engine").height()
+		}
+});
