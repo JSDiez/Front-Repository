69be58785b2cb28743620db3461886bf256f6d19
Jesús S.D. <jesus.silla@miraiespana.com>
2014-10-20 14:57:14 +0200

    Location - final version

--- Hotel-Alfonso-VI/custom.js
+++ Hotel-Alfonso-VI/custom.js
@@ -2,8 +2,15 @@
  * Smooth scrolling for nav sections.
  */
 Mirai.query('#engine a').click(function(evt){
-  evt.preventDefault();
-  var link = Mirai.query(this).attr('href');
-  var pos = Mirai.query(link).offset().top;
-  Mirai.query('html,body').animate({scrollTop:pos-50},700);
-}); 
+    evt.preventDefault();
+	var link = Mirai.query(this).attr('href');
+	var pos = Mirai.query(link).offset().top;
+	Mirai.query('html,body').animate({scrollTop:pos-50},700);
+});
+
+
+Mirai.query('#engine_mashup').affix({
+	offset: {
+	top: Mirai.query("#header").height() + Mirai.query("#engine").height()
+	}
+});
