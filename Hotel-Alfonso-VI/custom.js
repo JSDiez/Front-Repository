ce0bea9637a51bce09d631e0a401c7f994f630c0
Jesús S.D. <jesus.silla@miraiespana.com>
2014-10-20 15:20:14 +0200

    Add them to Next Gen Gallery
    - Html
    - CSS
    - Js

--- Hotel-Alfonso-VI/custom.js
+++ Hotel-Alfonso-VI/custom.js
@@ -2,15 +2,33 @@
  * Smooth scrolling for nav sections.
  */
 Mirai.query('#engine a').click(function(evt){
-    evt.preventDefault();
-	var link = Mirai.query(this).attr('href');
-	var pos = Mirai.query(link).offset().top;
-	Mirai.query('html,body').animate({scrollTop:pos-50},700);
+  evt.preventDefault();
+  var link = Mirai.query(this).attr('href');
+  var pos = Mirai.query(link).offset().top;
+  Mirai.query('html,body').animate({scrollTop:pos-50},700);
 });
 
 
 Mirai.query('#engine_mashup').affix({
-	offset: {
-	top: Mirai.query("#header").height() + Mirai.query("#engine").height()
-	}
+		offset: {
+		top: Mirai.query("#header").height() + Mirai.query("#engine").height()
+		}
+});
+
+
+function addNumOfPhotos () {
+  jQuery('#main').find('.photo_title').each(function(){
+	var NumPhotos = jQuery(this).find('.ngg-gallery-thumbnail-box').length;
+	jQuery(this).find('.n_photos').text('/'+NumPhotos);
+    jQuery(this).find('.ngg-gallery-thumbnail-box:first-child').addClass('first-child');
+    if(NumPhotos <= 1){
+    	jQuery(this).find('.n_photos').css('display', 'none');
+    }
+  })
+}
+
+jQuery(document).ready(function(){  
+    if(jQuery('.ngg-galleryoverview').length > 0) {
+      addNumOfPhotos ();
+    };  
 });
