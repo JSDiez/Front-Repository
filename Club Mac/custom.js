7213aa1bce91fa3fd96ff7a241f608c189cf10eb
Jesús S.D. <jesus.silla@miraiespana.com>
2014-11-25 10:21:31 +0100

    Change names

--- Club Mac/custom.js
+++ Club Mac/custom.js
@@ -1,38 +1,37 @@
 var mainmenu = function() {
-  /* Push the body and the nav over by 285px over */
-	  jQuery('.ico-menu-movil').click(function() {
-		jQuery('#menu_web').css('display', 'block');
-		jQuery('#menu_web').animate({
-		  right: "-0px"
-		}, 200);
+/* Push the body and the nav over by 285px over */
+  jQuery('.ico-menu-movil').click(function() {
+	jQuery('#menu_web').css('display', 'block');
+	jQuery('#menu_web').animate({
+	  right: "-0px"
+	}, 200);
 
-		jQuery('.ico-menu-movil').animate({
-		  right: "-200px"
-		}, 200);
-        
-        jQuery('.ico-close-movil').animate({
-		  top: "43px"
-		}, 200);
-	  });
+	jQuery('.ico-menu-movil').animate({
+	  right: "-200px"
+	}, 200);
+    
+    jQuery('.ico-close-movil').animate({
+	  top: "43px"
+	}, 200);
+  });
 
-	  /* Then push them back */
-	  jQuery('.ico-close-movil').click(function() {
-		jQuery('#menu_web').css('display', 'none');
-		jQuery('#menu_web').animate({
-		  right: "-320px"
-		}, 200);
-		
-		jQuery('.ico-menu-movil').animate({
-		  right: "0"
-		}, 200);
-        
-        jQuery('.ico-close-movil').animate({
-		  top: "-10px"
-		}, 200);
-	  });
-	};
+  /* Then push them back */
+  jQuery('.ico-close-movil').click(function() {
+	jQuery('#menu_web').css('display', 'none');
+	jQuery('#menu_web').animate({
+	  right: "-320px"
+	}, 200);
 	
-jQuery(document).ready(mainmenu);
+	jQuery('.ico-menu-movil').animate({
+	  right: "0"
+	}, 200);
+    
+    jQuery('.ico-close-movil').animate({
+	  top: "-10px"
+	}, 200);
+  });
+};
+
 
 
 
@@ -49,13 +48,16 @@ function addNumOfPhotos () {
 }
 
 
+
+
+
 /* FULL SCREEN */
 var wHeight = jQuery( window ).height();
 var wWhidth = jQuery( window ).width();
-jQuery("#recption").backstretch("/files/reception-image.jpg");
+jQuery("#recption").backstretch("/files/reception-image01.jpg");
 jQuery("#tecnology").backstretch("/files/tecnology-image.jpg");
 jQuery("#shopping").backstretch("/files/shop-image.jpg");
-jQuery("#water").backstretch("/files/water-image.jpg");
+jQuery("#water").backstretch("/files/water-image-v2.jpg");
 jQuery("#sport").backstretch("/files/sports-image.jpg");
 jQuery("#entertainment").backstretch("/files/entertainment-image.jpg");
 jQuery("#beauty").backstretch("/files/beauty-image.jpg");
@@ -71,6 +73,7 @@ jQuery('#view-experience').click(function(e) {
       width: '100%',
       height: wHeight
     });
+	jQuery('#full-screen').css("z-index", '100');
 });
 
 // Back to web
@@ -81,8 +84,8 @@ jQuery('#back-to-web a').click(function() {
       overflow: 'auto',
       height: 'auto'
     });
+	jQuery('#full-screen').css("z-index", '-1');
 });
-
 
 // Navigation the content
 jQuery('#nav-slide a').click(function() {
@@ -105,6 +108,8 @@ jQuery('.info-dots li').bind({
 });
 
 
+
+
 // Fix engine position in scroll
 Mirai.query( document ).ready(function() {
   Mirai.query('.affix-top').affix({
@@ -116,4 +121,6 @@ Mirai.query( document ).ready(function() {
   if(jQuery('.ngg-galleryoverview').length > 0) {
     addNumOfPhotos ();
   };
+
+  mainmenu();
 })
